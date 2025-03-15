24bc86eed9579544ca70bf7d05019a66a2e28fa3
plmex <php.eleuterio@gmail.com>
2025-03-15 18:38:35 -0300

    feat: adiciona impar-par.py

--- impar-par.py
+++ impar-par.py
@@ -33,3 +33,29 @@ else:
 
 
 
+confirmacao = str(input("Deseja continuar? Aperte 'S' para sim e 'N' para não: "))
+
+while confirmacao not in ["N", "n", "não", "Não", "NÃO", "nao", "Nao", "NAO"]:
+
+    if confirmacao in ["S", "s", "sim", "Sim"]:
+
+        numero = int(input("Em qual número você pensou? \nNúmero: "))
+
+        res = numero % 2
+
+        if res == 0:
+            print(f"\033[7;36m{numero} é par!\033[m")
+        else: 
+            print(f"\033[7;35m{numero} é ímpar!\033[m")
+
+        confirmacao = str(input("Deseja continuar? Aperte 'S' para sim e 'N' para não: "))
+    
+
+    elif confirmacao != "S" and confirmacao != "N":
+        print("Opção inválida. Tente novamente!")
+        confirmacao = str(input("Deseja continuar? Aperte 'S' para sim e 'N' para não: "))
+
+print("\n")
+print("=" * 12)
+print("Fim de jogo!")
+print("=" * 12)
